50b2615446f213c56488deddb63bfc294aeaa2c3
lccfonseca <lccfonseca@gmail.com>
2023-06-07 16:45:14 -0300

    Configuração inicial do FastAPI

--- src/pcd_api/main.py
+++ src/pcd_api/main.py
@@ -1,1 +1,21 @@
-print('Oi')
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pcd_api.core.config import Settings
+
+app = FastAPI(title=Settings.project_name)
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"mensagem": "WSPcd API!"}
