de63efd5a1cb218ee7c85653d2710caa3bcee5c4
lccfonseca <lccfonseca@gmail.com>
2023-06-15 16:26:25 -0300

    Ajuste de conexão com o BD

--- src/pcd_api/api/v1/endpoints/deficiencia.py
+++ src/pcd_api/api/v1/endpoints/deficiencia.py
@@ -8,10 +8,10 @@ from pcd_api.repository.deficiencia_repository import list_deficiencias
 router = APIRouter()
 
 @router.get("/")
-async def list_all_health_insurer(db: Session = Depends(get_db)):
+async def list_all_deficiencia(db: Session = Depends(get_db)):
     try:        
-        health_insurer = list_deficiencias(db=db)
+        deficiencia = list_deficiencias(db=db)
     except Exception:
         raise HTTPException(status_code=500, detail="Erro ao buscar lista de deficiencias")
     else:
-        return health_insurer
+        return deficiencia
