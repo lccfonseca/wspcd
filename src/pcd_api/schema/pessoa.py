ddb72ca1ebb49e4e069c7d65f2f75ecb9b2f20f7
Márcio <mchcito@gmail.com>
2023-06-28 16:21:28 -0300

    Atualições das rotas pessoa

---
+++ src/pcd_api/schema/pessoa.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class PessoaCreate(BaseModel):
+    nome : str
+    cpf : int
+    email : str
+    cid : str
+    senha : str
+    dt_cadastro : str
+    deficiencia_id : int
+
+class PessoaShow(BaseModel):
+    id : int
+    nome : str
+    cpf : int
+    email : str
+    cid : str
+    senha : str
+    dt_cadastro : str
+    deficiencia_id : int
+    
+    class Config():  #tells pydantic to convert even non dict obj to json
+        orm_mode = True
