2c2c1ef99f5b58afa4a0d9e87b73aa14f4985cb2
unknown <chrisviana757@gmail.com>
2023-07-06 13:54:46 -0300

    atualização do ip

--- src/pcd_api/schema/pessoa.py
+++ src/pcd_api/schema/pessoa.py
@@ -14,12 +14,12 @@ class PessoaCreate(BaseModel):
 
 class PessoaShow(BaseModel):
     id: int
-    nome : str
-    cpf : int
-    email : str
-    cid : str
-    senha : str
-    dt_cadastro : date
-    deficiencia_id : int
+    nome: str
+    cpf: int
+    email: str
+    cid: str
+    senha: str
+    dt_cadastro: date
+    deficiencia_id: int
     class Config():  # tells pydantic to convert even non dict obj to json
         orm_mode = True
