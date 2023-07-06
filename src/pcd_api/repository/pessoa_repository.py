c7681a2eafe4a0e3e75f9179b7c339f106cd93a3
Caiovicrz <caiovicrz@outlook.com>
2023-07-06 17:31:34 -0300

    atualização de pessoas

--- src/pcd_api/repository/pessoa_repository.py
+++ src/pcd_api/repository/pessoa_repository.py
@@ -1,4 +1,3 @@
-
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
@@ -9,8 +8,59 @@ from pcd_api.model.pessoa import Pessoa
 def list_pessoas(db: Session):
     with db:
         try:
-            pessoa = db.query(Pessoa).all()
-            return pessoa
+            health_insurer = db.query(Pessoa).all()
+            return health_insurer
         except Exception as err:
-            #logger.error(st.project_name+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
-            return HTTPException(status_code = 500, detail = f"Erro na conexão com o Banco de Dados!")
+            # logger.error(st.project_name+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
+            return HTTPException(status_code=500, detail=f"Erro na conexão com o Banco de Dados!")
+
+
+def retrieve_pessoa_by_id(id: int, db: Session):
+    with db:
+        try:
+            item = db.query(Pessoa).filter(Pessoa.id == id).first()
+            return item
+        except Exception as err:
+            # logger.exception(st.PROJECT_NAME+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
+            return err
+
+
+def create_new_pessoa(pessoa: Pessoa, db: Session):
+    with db:
+        try:
+            pessoa_obj = Pessoa(**pessoa.dict())
+            db.add(pessoa_obj)
+            db.commit()
+            db.refresh(pessoa_obj)
+            return pessoa_obj
+        except Exception as err:
+            # logger.exception(st.PROJECT_NAME+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
+            return err
+
+
+def update_pessoa_by_id(id: int, pessoa: Pessoa, db: Session):
+    with db:
+        try:
+            existing_pessoa = db.query(Pessoa).filter(Pessoa.id == id)
+            if not existing_pessoa.first():
+                return 0
+            existing_pessoa.update(pessoa._dict_)
+            db.commit()
+            return 1
+        except Exception as err:
+            # logger.exception(st.PROJECT_NAME+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
+            return err
+
+
+def delete_pessoa_by_id(id: int, db: Session):
+    with db:
+        try:
+            existing_pessoa = db.query(Pessoa).filter(Pessoa.id == id)
+            if not existing_pessoa.first():
+                return 0
+            existing_pessoa.delete(synchronize_session=False)
+            db.commit()
+            return 1
+        except Exception as err:
+            # logger.exception(st.PROJECT_NAME+':::'+f"Erro na conexão com o Banco de Dados: " + str(err))
+            return err
